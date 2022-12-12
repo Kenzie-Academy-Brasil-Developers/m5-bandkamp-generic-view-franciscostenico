cbfcfbc4dda543cbe9ed2e229c022a8e59539a09
Francisco Stenico <francisco.stenico@gmail.com>
2022-12-12 19:27:09 -0300

    REFACT: Serializer > ModelSerializer

--- users/models.py
+++ users/models.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=64, unique=True)
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+    objects = UserManager()
