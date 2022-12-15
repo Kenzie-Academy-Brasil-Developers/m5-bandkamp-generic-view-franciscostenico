10efc4857098cd05ed2df08d0ef2d1a87b11f878
Francisco Stenico <francisco.stenico@gmail.com>
2022-12-15 10:03:44 -0300

    FIX: Correção de retorno em /albums/<int:pk>/songs
    - Foi corrigida a resposta no caso da "pk" passada na URL estiver
    referenciando um "id" nulo.
    Retorno antigo: Retornava o HTTP padrão do Django
    Retorno atual: Retorna um JSON com a chave detail e a mensagem de "not found."

--- songs/views.py
+++ songs/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.pagination import PageNumberPagination
+
+from albums.models import Album
 
 from .models import Song
 from .serializers import SongSerializer
@@ -15,4 +18,5 @@ class SongView(ListCreateAPIView, PageNumberPagination):
     queryset = Song.objects.all()
 
     def perform_create(self, serializer: SongSerializer):
-        return serializer.save(album_id=self.kwargs["pk"])
+        album = get_object_or_404(Album, id=self.kwargs["pk"])
+        return serializer.save(album_id=album.id)
